993854a3040475138568fdbaba4d00234c5cd45f
idurar <lalami.sdn@gmail.com>
2022-01-16 11:36:46 +0100

    update server

--- graphql/typeDefs/index.ts
+++ graphql/typeDefs/index.ts
@@ -1,6 +1,9 @@
 import { mergeSchemas } from 'fast-graphql';
+const path = require('path');
+// import { loadFilesSync } from '@graphql-tools/load-files';
 
-const pathfiles = './graphql/typeDefs/*.gql';
+const pathfiles = 'graphql/typeDefs/*.gql';
+console.log('🚀 ~ file: index.ts ~ line 6 ~ pathfiles', pathfiles);
 
 const mergedSchemas = mergeSchemas({ pathfiles });
 
